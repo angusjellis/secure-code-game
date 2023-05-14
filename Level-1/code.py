ef9cc26cfc0db513fb7c8bcacf9bd05e1e2f5629
Angus Jellis <angusjellis@live.com>
2023-05-14 15:44:53 +0000

    let's attempt a fix

--- Level-1/code.py
+++ Level-1/code.py
@@ -12,20 +12,33 @@
 '''
 
 from collections import namedtuple
+from decimal import Decimal
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
+
+MAX_ITEM_PRICE = 60000
+MAX_ITEM_QUANTITY = 50
+MAX_ORDER_VALUE = 3e6
 
 def validorder(order: Order):
     net = 0
     
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            payment_compliant = item.amount > -1 * MAX_ITEM_PRICE and item.amount < MAX_ITEM_PRICE
+            if payment_compliant: 
+                net += item.amount
+            else:
+                print(f"Attempting a payment higher than {MAX_ITEM_PRICE} is not allowed.")
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            item_compliant = item.quantity > 0 and item.quantity <= MAX_ITEM_QUANTITY and item.amount > 0 and item.amount <= MAX_ITEM_PRICE
+            if item_compliant:
+                net -= item.amount * item.quantity
+                if net > MAX_ORDER_VALUE or net < -1*MAX_ORDER_VALUE:
+                    return("Total amount exceeded")
         else:
-            return("Invalid item type: %s" % item.type)
+            return(f"Invalid item type: {item.type}")
     
     if net != 0:
         return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
